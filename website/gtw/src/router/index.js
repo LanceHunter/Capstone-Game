9648ce64d0d526b7ca4ce641c8e7eeb81fefa95f
Gavin Loughridge <gavinlough@gmail.com>
2018-03-15 15:49:52 -0500

    created simple routes

--- website/gtw/src/router/index.js
+++ website/gtw/src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Home from '@/views/Home';
+import Leaderboard from '@/views/Leaderboard';
+import Login from '@/views/Login';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/leaderboard',
+      name: 'leaderboard',
+      component: Leaderboard,
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
     },
   ],
 });
