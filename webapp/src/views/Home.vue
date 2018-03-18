1f46f4650e4cef61254f17a41d71c1833d23c2a1
Steven Fiero <ssfiero@yahoo.com>
2018-03-18 01:07:03 -0500

    Webapp - home & sigin & leaderboard pages almost complete

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -183,13 +183,13 @@
 
 <script>
 // Jquery tabs
-$(document).ready(function() {
-  $('#tab_header ul li.item').on('click', function() {
-    var number = $(this).data('option');
-    $('#tab_header ul li.item').removeClass('is-active');
-    $(this).addClass('is-active');
-    $('#tab_container .container_item').removeClass('is-active');
-    $('div[data-item="' + number + '"]').addClass('is-active');
+$(document).ready(function jqtabs() { // eslint-disable-line
+  $('#tab_header ul li.item').on('click', function jqtabscl() { // eslint-disable-line
+    const number = $(this).data('option'); // eslint-disable-line
+    $('#tab_header ul li.item').removeClass('is-active'); // eslint-disable-line
+    $(this).addClass('is-active'); // eslint-disable-line
+    $('#tab_container .container_item').removeClass('is-active'); // eslint-disable-line
+    $('div[data-item="' + number + '"]').addClass('is-active'); // eslint-disable-line
   });
 });
 
