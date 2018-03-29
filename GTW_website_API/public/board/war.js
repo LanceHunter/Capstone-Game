5c05d109e545b8583c3779aec35e17c880676b3a
Jason Amador <jason@Jasons-MacBook-Pro.local>
2018-03-29 15:32:53 -0500

    fix thing

--- GTW_website_API/public/board/war.js
+++ GTW_website_API/public/board/war.js
@@ -91,7 +91,7 @@ let war = {
 
     // look at every pointer
     pointers.forEach((pointer) => {
-      // pointer.setPosition();
+      pointer.setPosition();
 
       if (!pointer.intersecting()) {
         capitalIcons.some((capital) => {
