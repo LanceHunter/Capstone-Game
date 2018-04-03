618d8bd44997ff6dba2e9d76d40e9a4b8ac0593c
Jason Amador <jason.amador@gmail.com>
2018-04-03 14:47:12 -0500

    commented up all the icons, launch and all it's components are next

--- GTW_website_API/public/board/gameObjects.js
+++ GTW_website_API/public/board/gameObjects.js
@@ -100,7 +100,7 @@ TODO: refactor with inheritance
 */
 class SubIcon {
   constructor(x, y, ocean, playerID, phaserState) {
-    // our phaserState.game object, where we add new game objects
+    // where we add game objects, game state's 'this.game'
     this.phaserState = phaserState;
 
     // this is for the generalization of the Launch object, it handles different launches differently instead of having SubLaunch, BomberLaunch, MissileLaunch
@@ -142,8 +142,9 @@ class SubIcon {
         // in wartime, we enable input for the weapon sprites
         this.sprite.inputEnabled = true;
 
-        // and display the total inventory instead of the declared
+        // and display the total inventory instead of the declared, and centered
         this.inventory.setText(game.oceans[this.ocean].subs[this.playerID].total);
+        this.inventory.position.x = this.sprite.centerX - (this.inventory.width / 2);
 
         // if they are out of ammo
         if (game.oceans[this.ocean].subs[this.playerID].total <= 0) {
@@ -156,13 +157,14 @@ class SubIcon {
         }
       } else {
         this.inventory.setText(game.oceans[this.ocean].subs[this.playerID].declared);
+        this.inventory.position.x = this.sprite.centerX - (this.inventory.width / 2);
 
-        // if they are out of ammo
+        // if they have nothing declared, low alpha
         if (game.oceans[this.ocean].subs[this.playerID].declared <= 0) {
           this.sprite.alpha = deadAlpha;
           this.inventory.alpha = deadAlpha;
         } else {
-          // if they can shoot
+          // otherwise high alpha
           this.sprite.alpha = liveAlpha;
           this.inventory.alpha = liveAlpha;
         }
@@ -176,7 +178,7 @@ class SubIcon {
 
   // called on every game loop iteration
   update() {
-    // if it has a launch, update that launch
+    // if it has a launch, update it
     if (this.launch) {
       this.launch.update();
     }
@@ -207,10 +209,17 @@ class SubIcon {
 
 class BomberIcon {
   constructor(x, y, continent, phaserState) {
+    // where we add game objects, game state's 'this.game'
     this.phaserState = phaserState;
+
+    // this is for the generalization of the Launch object, it handles different launches differently instead of having SubLaunch, BomberLaunch, MissileLaunch
     this.type = 'bomber';
+
+    // we need to know about the continent and owner
     this.continent = continent;
     this.playerID = Object.keys(game.continents[this.continent].player)[0];
+
+    // sprite setup
     this.sprite = this.phaserState.add.sprite(x, y, 'bomber');
     this.sprite.anchor.set(0, 1);
     this.sprite.tint = colors[playerIDs.indexOf(this.playerID)];
@@ -224,44 +233,57 @@ class BomberIcon {
   updateState() {
     //check for game state and update inventory accordingly
     if (game.war) {
+      // in wartime, we enable input for the weapon sprites
+      this.sprite.inputEnabled = true;
+
+      // and display the total inventory instead of the declared, and centered
       this.inventory.setText(game.continents[this.continent].forces.bombers.total);
       this.inventory.position.x = this.sprite.centerX - (this.inventory.width / 2);
-      this.sprite.inputEnabled = true;
+
       // if they are out of ammo
       if (game.continents[this.continent].forces.bombers.total <= 0) {
         this.sprite.alpha = deadAlpha;
         this.inventory.alpha = deadAlpha;
       } else {
+        // if they can shoot
         this.sprite.alpha = liveAlpha;
         this.inventory.alpha = liveAlpha;
       }
     } else {
+      // in peacetime, display the declared forces
       this.inventory.setText(game.continents[this.continent].forces.bombers.declared);
       this.inventory.position.x = this.sprite.centerX - (this.inventory.width / 2);
-      // if they are haven't declared anything
+
+      // if they have nothing declared, low alpha
       if (game.continents[this.continent].forces.bombers.declared <= 0) {
         this.sprite.alpha = deadAlpha;
         this.inventory.alpha = deadAlpha;
       } else {
+        // otherwise high alpha
         this.sprite.alpha = peaceAlpha;
         this.inventory.alpha = peaceAlpha;
       }
     }
   }
 
+  // called on every game loop iteration
   update() {
+    // if it has a launch, update it
     if (this.launch) {
       this.launch.update();
     }
   }
 
   select(data) {
+    // self replaces the intuitive 'this' for this method
     let self = data.self;
     let pointer = data.pointer;
 
     // is it your bomber?
     if (self.playerID === pointer.playerID) {
+      // do you have ammo?
       if (game.continents[self.continent].forces.bombers.total > 0) {
+        // is there already a launch happening?
         if (!self.launch) {
           self.launch = new Launch(self, self.phaserState);
         } else {
@@ -276,10 +298,17 @@ class BomberIcon {
 
 class MissileIcon {
   constructor(x, y, continent, phaserState) {
+    // where we add game objects, game state's 'this.game'
     this.phaserState = phaserState;
+
+    // this is for the generalization of the Launch object, it handles different launches differently instead of having SubLaunch, BomberLaunch, MissileLaunch
     this.type="icbm";
+
+    // we need to know about the continent and owner
     this.continent = continent
     this.playerID = Object.keys(game.continents[this.continent].player)[0];
+
+    // sprite setup
     this.sprite = this.phaserState.add.sprite(x, y, 'missile');
     this.sprite.anchor.set(0, 1);
     this.sprite.tint = colors[playerIDs.indexOf(this.playerID)];
@@ -293,47 +322,57 @@ class MissileIcon {
   updateState() {
     //check for game state and update inventory accordingly
     if (game.war) {
+      // in wartime, we enable input for the weapon sprites
+      this.sprite.inputEnabled = true;
+
+      // and display the total inventory instead of the declared, and centered
       this.inventory.setText(game.continents[this.continent].forces.icbms.total);
       this.inventory.position.x = this.sprite.centerX - (this.inventory.width / 2);
-      this.sprite.inputEnabled = true;
 
       // if they are out of ammo
       if (game.continents[this.continent].forces.icbms.total <= 0) {
         this.sprite.alpha = deadAlpha;
         this.inventory.alpha = deadAlpha;
       } else {
+        // if they can shoot
         this.sprite.alpha = liveAlpha;
         this.inventory.alpha = liveAlpha;
       }
     } else {
+      // in peacetime, display declared forces
       this.inventory.setText(game.continents[this.continent].forces.icbms.declared);
       this.inventory.position.x = this.sprite.centerX - (this.inventory.width / 2);
 
-      // if they are out of ammo
+      // if they have nothing declared, low alpha
       if (game.continents[this.continent].forces.icbms.declared <= 0) {
         this.sprite.alpha = deadAlpha;
         this.inventory.alpha = deadAlpha;
       } else {
+        // otherwise high alpha
         this.sprite.alpha = liveAlpha;
         this.inventory.alpha = liveAlpha;
       }
     }
   }
 
+  // called every game loop iteration
   update() {
+    // if it has a launch, update it
     if (this.launch) {
       this.launch.update();
     }
   }
 
   select(data) {
+    // self replaces the intuitive 'this' for this method
     let self = data.self;
     let pointer = data.pointer;
 
     // is it your missile?
     if (self.playerID === pointer.playerID) {
+      // do you have ammo?
       if (game.continents[self.continent].forces.icbms.total > 0) {
-        // console.log(self.launch);
+        // is there already a launch happening?
         if (!self.launch) {
           self.launch = new Launch(self, self.phaserState);
         } else {
